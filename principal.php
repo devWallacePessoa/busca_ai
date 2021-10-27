0b1dbc72531ce19fd12968179288a3eac08c013f
Samuel Teixeira Dias <83990486+srTeixeiraDias@users.noreply.github.com>
2021-10-27 14:12:47 -0300

    mudando a pagina principal

--- principal.php
+++ principal.php
@@ -11,7 +11,7 @@
 <nav class="navbar fixed-top navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#" id="navlogo"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
-        <a class="navbar-brand" face="Malgun Gothic" href="#">Sobre nós</a>
+        <a class="navbar-brand" face="Malgun Gothic" href="#"> <input type="text" placeholder="Produto Desejado" name="pesquisa" id="pesquisa">  </a>
       </div>
     </nav>
 
@@ -23,8 +23,17 @@
       <h1 class="fw-light">LISTA DE PRODUTOS</h1>
       <p class="lead text-muted">Aqui você encontrará o seu produto desejado nos comercios locais.</p>
       <p>
-        <a href="#" class="btn btn-primary my-2">الدعوة الرئيسية للعمل</a>
-        <a href="#" class="btn btn-secondary my-2">عمل ثانوي</a>
+      <div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Categorias
+  </a>
+
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Alguma ação</a>
+    <a class="dropdown-item" href="#">Outra ação</a>
+    <a class="dropdown-item" href="#">Alguma coisa aqui</a>
+  </div>
+</div>
       </p>
     </div>
   </div>
@@ -36,97 +45,48 @@
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       <div class="col">
         <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
 
           <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
+            <p class="card-text">O titulo do seu produto vai</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
               </div>
-              <small class="text-muted">9 دقائق</small>
+              <small class="text-muted">Hora da publicação</small>
             </div>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
 
           <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
+            <p class="card-text">O titulo do seu produto vai aqui</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
+               <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
               </div>
-              <small class="text-muted">9 دقائق</small>
+              <small class="text-muted">Hora da publicação</small>
             </div>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
 
           <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
+                 <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
               </div>
-              <small class="text-muted">9 دقائق</small>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
-              </div>
-              <small class="text-muted">9 دقائق</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
-              </div>
-              <small class="text-muted">9 دقائق</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
-              </div>
-              <small class="text-muted">9 دقائق</small>
+              <small class="text-muted">Hora da publicação</small>
             </div>
           </div>
         </div>
@@ -134,48 +94,97 @@
 
       <div class="col">
         <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
 
           <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
               </div>
-              <small class="text-muted">9 دقائق</small>
+              <small class="text-muted">Hora da publicação</small>
             </div>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
 
           <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
               </div>
-              <small class="text-muted">9 دقائق</small>
+              <small class="text-muted">Hora da publicação</small>
             </div>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
 
           <div class="card-body">
-            <p class="card-text">هذه بطاقة أوسع مع نص داعم أدناه كمقدمة طبيعية لمحتوى إضافي. هذا المحتوى أطول قليلاً.</p>
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">عرض</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">تعديل</button>
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
               </div>
-              <small class="text-muted">9 دقائق</small>
+              <small class="text-muted">Hora da publicação</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card shadow-sm">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
+
+          <div class="card-body">
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
+              </div>
+              <small class="text-muted"> Hora da publicação</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
+
+          <div class="card-body">
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
+              </div>
+              <small class="text-muted">Hora da publicação</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow-sm">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">IMAGEM DO PRODUTO</text></svg>
+
+          <div class="card-body">
+            <p class="card-text">O titulo do seu produto vai aqui.</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-primary">Tenho Interesse</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Ofertar Agora</button>
+              </div>
+              <small class="text-muted">Hora da publicação</small>
             </div>
           </div>
         </div>
