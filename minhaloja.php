344a4d96f277ab09be07c3dfc46c4ff19e1d601e
Vini-png <viniciusjesus.99@hotmail.com>
2021-11-27 23:04:26 -0300

    Alterando função da imagem

--- minhaloja.php
+++ minhaloja.php
@@ -7,9 +7,36 @@
 </head>
 <body>
 
-<div>
+<nav class="navbar navbar-expand-lg navbar navbar-dark fixed-top bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="principal.php"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="./principal.php">Principal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="minhaloja.php">Minha Loja</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./faleconosco.php">Fale Conosco</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  href="./sobrenos.php">Sobre Nós</a>
+        </li>
+      </ul>
 
-</div>
+    </div>
+  </div>
+</nav>
+
+
+<p></p>.
+<p></p>.
+
 
 <?php include "bootstrap.php"; include "rodape.php" ?>
 
