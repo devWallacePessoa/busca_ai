0425e641d14d468c6674361906bff121761a42eb
devWallacePessoa <83680528+devWallacePessoa@users.noreply.github.com>
2021-12-02 21:57:10 -0300

    Add files via upload

--- minhaloja.php
+++ minhaloja.php
@@ -1,9 +1,12 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Sobre Nos</title>
+    <?php include"bootstrap.php"?>
 </head>
 <body>
 
@@ -13,32 +16,40 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="principal.php"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
+      <a class="navbar-brand" href="./principal.php"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="./principal.php">Principal</a>
+          <a class="nav-link " aria-current="page" href="./principal.php">Principal</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="minhaloja.php">Minha Loja</a>
+          <a class="nav-link" aria-current="page" href="#">Minha Loja</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="./faleconosco.php">Fale Conosco</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link"  href="./sobrenos.php">Sobre Nós</a>
+          <a class="nav-link active"  href="./sobrenos.php">Sobre Nós</a>
         </li>
       </ul>
 
     </div>
   </div>
 </nav>
+<br>
+<br><br><br><center><h4>Nome da loja</h4></center>
 
+<div id="ml">
+  <center><h5>Descrição</h5></center>
+  <p>ExemploExemploExemploExemploExemploExemploExemploExemploExemploExemploExemploExemplo<br>ExemploExemploExemplo</p>
+  <p>ExemploExemploExemploExemploExemploExemploExemploExemploExemploExemploExemploExemplo<br>ExemploExemploExemplo</p>
+</div>
+<div id="ml1"><center><img src="imagens/mercado.png" height="117" width="117"></center>
+<p><h5>Endereço:</h5>Rua, Cidade, Estado
+(00000-000)</p> 
+<p><h5>Exemplo:</h5>exemploexemploexemplo</p>
+</div>
 
-<p></p>.
-<p></p>.
-
-
-<?php include "bootstrap.php"; include "rodape.php" ?>
+<?php include "rodape.php"?>
 
 </body>
 </html>
